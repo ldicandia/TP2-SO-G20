d156dfb63c0564fc0ad241b02dfcb5a44ae0b4a8
procerr <128986084+ldicandia@users.noreply.github.com>
2024-10-18 14:38:43 -0300

    added files

--- x64BareBones-master/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Compilando..."
-sudo docker start TPE_2024
-sudo docker exec -it TPE_2024 make clean -C /root/Toolchain
-sudo docker exec -it TPE_2024 make clean -C /root/
-sudo docker exec -it TPE_2024 make -C /root/Toolchain
-sudo docker exec -it TPE_2024 make -C /root/
-sudo docker stop TPE_2024
-#sudo docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -it --name TPE_2024 agodio/itba-so:2.0
