e9ae903be547a78abf2eeb744e67c580c0d6304d
lucasdicandi <ldicandia@itba.edu.ar>
2025-04-25 12:44:06 -0300

    Include Process and added provisionary test

---
+++ TPE_ARQUI/Kernel/processes/process.c
@@ -0,0 +1,92 @@
+#include <defs.h>
+#include <interrupts.h>
+#include <lib.h>
+#include <memoryManager.h>
+#include <process.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STACK_SIZE (1 << 12) // 4KB stack size
+
+static char **allocArguments(char **args) {
+	int argc = stringArrayLen(args), totalArgsLen = 0;
+	int argsLen[argc];
+	for (int i = 0; i < argc; i++) {
+		argsLen[i] = strlen(args[i]) + 1;
+		totalArgsLen += argsLen[i];
+	}
+	char **newArgsArray =
+		(char **) malloc(totalArgsLen + sizeof(char **) * (argc + 1));
+	char *charPosition = (char *) newArgsArray + (sizeof(char **) * (argc + 1));
+	for (int i = 0; i < argc; i++) {
+		newArgsArray[i] = charPosition;
+		memcpy(charPosition, args[i], argsLen[i]);
+		charPosition += argsLen[i];
+	}
+	newArgsArray[argc] = NULL;
+	return newArgsArray;
+}
+
+static void assignFileDescriptor(Process *process, uint8_t fdIndex,
+								 int16_t fdValue, uint8_t mode) {
+	process->fileDescriptors[fdIndex] = fdValue;
+	if (fdValue >= BUILT_IN_DESCRIPTORS)
+		pipeOpenForPid(process->pid, fdValue, mode);
+}
+
+void initProcess(Process *process, uint16_t pid, uint16_t parentPid,
+				 char **args, char *name, uint8_t priority,
+				 int16_t fileDescriptors[], uint8_t unkillable) {
+	process->pid		   = pid;
+	process->parentPid	   = parentPid;
+	process->waitingForPid = 0;
+	process->stackBase	   = malloc(STACK_SIZE);
+	if (process->stackBase == NULL) {
+		process->status = DEAD;
+		return;
+	}
+	process->argv = allocArguments(args);
+	if (process->argv == NULL) {
+		free(process->stackBase);
+		process->status = DEAD;
+		return;
+	}
+	strcpy(process->name, name);
+	if (process->name != NULL) {
+		strcpy(process->name, name);
+	}
+	else {
+		free(process->argv);
+		free(process->stackBase);
+		process->status = DEAD;
+		return;
+	}
+
+	process->priority = priority;
+	void *stackEnd	  = (void *) ((uint64_t) process->stackBase + STACK_SIZE);
+
+	process->stackPos = NULL; // PLACEHOLDER. FALTA IMPLEMENTAR!!!
+
+	process->status = READY;
+
+	process->unkillable = unkillable;
+	process->retValue	= 0;
+
+	assignFileDescriptor(process, STDIN, fileDescriptors[STDIN], READ);
+	assignFileDescriptor(process, STDOUT, fileDescriptors[STDOUT], WRITE);
+	assignFileDescriptor(process, STDERR, fileDescriptors[STDERR], WRITE);
+}
+
+void freeProcess(Process *process) {
+	if (process == NULL)
+		return;
+
+	freeMemory(process->stackBase);
+	freeMemory(process->name);
+	freeMemory(process->argv);
+	freeMemory(process);
+}
+
+int processIsWaiting(Process *process, uint16_t pidToWait) {
+	return process->status == BLOCKED && process->waitingForPid == pidToWait;
+}
---
+++ TPE_ARQUI/Kernel/testing/MemoryManagerTest.c
@@ -0,0 +1,145 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "./include/CuTest.h"
+#include "../include/memoryManager.h"
+#include "./include/MemoryManagerTest.h"
+
+#define MANAGED_MEMORY_SIZE 20480
+#define ALLOCATION_SIZE 1024
+#define WRITTEN_VALUE 'a'
+
+void testAllocMemory(CuTest *const cuTest);
+void testTwoAllocations(CuTest *const cuTest);
+void testWriteMemory(CuTest *const cuTest);
+
+static const size_t TestQuantity	   = 3;
+static const Test MemoryManagerTests[] = {testAllocMemory, testTwoAllocations,
+										  testWriteMemory};
+
+static inline void givenAMemoryManager(CuTest *const cuTest);
+static inline void givenAMemoryAmount(void);
+static inline void givenAnAllocation(void);
+
+static inline void whenMemoryIsAllocated(void);
+static inline void whenMemoryIsWritten(void);
+
+static inline void thenSomeMemoryIsReturned(CuTest *const cuTest);
+static inline void thenTheTwoAdressesAreDifferent(CuTest *const cuTest);
+static inline void thenBothDoNotOverlap(CuTest *const cuTest);
+static inline void thenMemorySuccessfullyWritten(CuTest *const cuTest);
+
+static MemoryManagerADT memoryManager;
+
+static size_t memoryToAllocate;
+
+static void *allocatedMemory = NULL;
+static void *anAllocation	 = NULL;
+
+CuSuite *getMemoryManagerTestSuite(void) {
+	CuSuite *const suite = CuSuiteNew();
+
+	for (size_t i = 0; i < TestQuantity; i++)
+		SUITE_ADD_TEST(suite, MemoryManagerTests[i]);
+
+	return suite;
+}
+
+void testAllocMemory(CuTest *const cuTest) {
+	givenAMemoryManager(cuTest);
+	givenAMemoryAmount();
+
+	whenMemoryIsAllocated();
+
+	thenSomeMemoryIsReturned(cuTest);
+}
+
+void testTwoAllocations(CuTest *const cuTest) {
+	givenAMemoryManager(cuTest);
+	givenAMemoryAmount();
+	givenAnAllocation();
+
+	whenMemoryIsAllocated();
+
+	thenSomeMemoryIsReturned(cuTest);
+	thenTheTwoAdressesAreDifferent(cuTest);
+	thenBothDoNotOverlap(cuTest);
+}
+
+void testWriteMemory(CuTest *const cuTest) {
+	givenAMemoryManager(cuTest);
+	givenAMemoryAmount();
+	givenAnAllocation();
+
+	whenMemoryIsWritten();
+
+	thenMemorySuccessfullyWritten(cuTest);
+}
+
+inline void givenAMemoryManager(CuTest *const cuTest) {
+	void *managedMemory = malloc(MANAGED_MEMORY_SIZE);
+	if (managedMemory == NULL) {
+		CuFail(cuTest, "[givenAMemoryManager] Managed Memory cannot be null");
+	}
+
+	memoryManager = createMemoryManager(managedMemory, MANAGED_MEMORY_SIZE);
+	if (memoryManager == NULL) {
+		CuFail(cuTest, "[givenAMemoryManager] Failed to create Memory Manager");
+	}
+}
+
+inline void givenAMemoryAmount(void) {
+	memoryToAllocate = ALLOCATION_SIZE;
+}
+
+inline void givenAnAllocation(void) {
+	anAllocation = allocMemory(memoryManager, memoryToAllocate);
+	if (anAllocation == NULL) {
+		fprintf(stderr, "Error: Memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+inline void whenMemoryIsAllocated(void) {
+	allocatedMemory = allocMemory(memoryManager, memoryToAllocate);
+	if (allocatedMemory == NULL) {
+		fprintf(stderr, "Error: Memory allocation failed.\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+inline void whenMemoryIsWritten(void) {
+	if (anAllocation == NULL) {
+		fprintf(stderr, "Error: Attempting to write to a null pointer.\n");
+		exit(EXIT_FAILURE);
+	}
+	*((char *) anAllocation) = WRITTEN_VALUE;
+}
+
+inline void thenSomeMemoryIsReturned(CuTest *const cuTest) {
+	CuAssertPtrNotNull(cuTest, allocatedMemory);
+}
+
+inline void thenTheTwoAdressesAreDifferent(CuTest *const cuTest) {
+	CuAssertTrue(cuTest, anAllocation != allocatedMemory);
+}
+
+inline void thenBothDoNotOverlap(CuTest *const cuTest) {
+	if (anAllocation == NULL || allocatedMemory == NULL) {
+		CuFail(cuTest, "[thenBothDoNotOverlap] Null pointer detected.");
+		return;
+	}
+
+	int distance = (char *) anAllocation - (char *) allocatedMemory;
+	distance	 = abs(distance);
+
+	CuAssertTrue(cuTest, distance >= ALLOCATION_SIZE);
+}
+
+inline void thenMemorySuccessfullyWritten(CuTest *const cuTest) {
+	uint8_t writtenValue = WRITTEN_VALUE;
+	uint8_t readValue	 = *((uint8_t *) anAllocation);
+
+	CuAssertIntEquals(cuTest, writtenValue, readValue);
+}
